79a7805d674703435089c057a6b27b67abda3d8c
emmaschultz <ecs89@case.edu>
2016-02-17 22:12:36 -0500

    Rough draft of what this should be

--- mobot_motion_control/src/mobot_action_client.cpp
+++ mobot_motion_control/src/mobot_action_client.cpp
@@ -1,13 +1,13 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include <nav_msgs/Path.h>
+//#include <nav_msgs/Path.h>
+#include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
 #include <mobot_motion_control/PathMsgAction.h>
 
 bool g_alarm_activated = false;
 
 // This function will be called once when the goal completes
-// this is optional, but it is a convenient way to get access to the "result" message sent by the server
 void doneCb(const actionlib::SimpleClientGoalState& state, const mobot_motion_control::PathMsgResultConstPtr& result) {
     ROS_INFO("doneCb: server responded with state [%s]", state.toString().c_str());
     bool isSuccess = result->completed;
@@ -18,7 +18,8 @@ void doneCb(const actionlib::SimpleClientGoalState& state, const mobot_motion_co
     }
 }
 
-void executeCb(const std_msgs::Bool& alarm_msg) {
+//this function will be called when lidar alarm information is received
+void alarmCb(const std_msgs::Bool& alarm_msg) {
     g_alarm_activated = alarm_msg.data;
     if (g_alarm_activated) {
         ROS_INFO("LIDAR alarm activated!");
@@ -32,7 +33,7 @@ int main(int argc, char** argv) {
 
     actionlib::SimpleActionClient<mobot_motion_control::PathMsgAction> action_client("mobot_action", true);
 
-    ros::Subscriber lidar_alarm_sub = nh.subscribe("lidar_alarm", 1, executeCb);  //subscriber listening to LIDAR alarm
+    ros::Subscriber lidar_alarm_sub = nh.subscribe("lidar_alarm", 1, alarmCb);  //subscriber listening to LIDAR alarm
 
     // attempt to connect to the server:
     ROS_INFO("waiting for server: ");
@@ -50,36 +51,50 @@ int main(int argc, char** argv) {
     //send these goal poses from the client to the server
     //make sure that nav_path.header.seq is filled in with a counter
     //then just use the loop to check whether alarm has been triggered?
+    ros::Rate timer(100);
 
     while(ros::ok()) {
         if(g_alarm_activated) {
             //preempt previously sent goal
-            action_client.cancelAllGoals();
-            //action_client.cancelGoal();
+            //action_client.cancelAllGoals();
+            action_client.cancelGoal();
 
-            //give the mobot a new goal
+            //TODO MAKE THIS TURN
+
             g_alarm_activated = false; //reset alarm
         } else {
-            ////////do calculations and such here
-            //make a path of subgoals (maybe just have it go straight?)
-            //stuff this into nav_path message and send this to the server
-            //but first check if alarm has been tripped
+        	//set up goal distances
+        	goal.distance.resize(4);
+        	goal.distance[0] = 4;
+        	goal.distance[1] = 3;
+        	goal.distance[2] = 4;
+        	goal.distance[3] = 5;
+
+        	//set up goal angles
+        	goal.angle.resize(4);
+        	goal.angle[0] = 0;
+        	goal.angle[1] = 3.1415 / 2;
+        	goal.angle[2] = 0;
+        	goal.angle[3] = -3.1415 / 2;
+
+        	action_client.sendGoal(goal, &doneCb);
+
+        	/*
+        	bool finished_before_timeout = action_client.waitForResult(ros::Duration(5.0));
+        	//bool finished_before_timeout = action_client.waitForResult(); // wait forever...
+        	if (!finished_before_timeout) {
+            	ROS_WARN("giving up waiting on result");
+            	return 1;
+        	} else {
+            	//if here, then server returned a result to us
+        	}
+        	*/
         }
 
-        //action_client.sendGoal(goal); // simple example--send goal, but do not specify callbacks
-        action_client.sendGoal(goal, &doneCb); // we could also name additional callback functions here, if desired
-        //    action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); //e.g., like this
-
-        bool finished_before_timeout = action_client.waitForResult(ros::Duration(5.0));
-        //bool finished_before_timeout = action_client.waitForResult(); // wait forever...
-        if (!finished_before_timeout) {
-            ROS_WARN("giving up waiting on result");
-            return 1;
-        } else {
-            //if here, then server returned a result to us
-        }
-
+        //action_client.sendGoal(goal, &doneCb); // we could also name additional callback functions here, if desired
+        //action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); //e.g., like this
         ros::spinOnce();
+        timer.sleep();
     }
     return 0;
 }
